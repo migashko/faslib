58007fb037b4b298cc91f5a65502cc87e7816140
migashko <migashko@gmail.com>
2012-03-23 19:24:40 +0400

    *

---
+++ fas/jsonrpc/invoke/tags.hpp
@@ -0,0 +1,18 @@
+#ifndef FAS_JSONRPC_INVOKE_TAGS_HPP
+#define FAS_JSONRPC_INVOKE_TAGS_HPP
+
+namespace fas{ namespace jsonrpc{
+
+struct _parse_incoming_;
+struct _process_incoming_;
+struct _parse_object_;
+struct _process_object_;
+
+struct _foreach_notify_;
+struct _foreach_request_;
+struct _foreach_error_;
+
+
+}}
+
+#endif
