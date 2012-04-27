5b9753b3d41e1373315e5809988f435898b72e15
migashko <migashko@gmail.com>
2012-04-28 02:32:35 +0400

    rename do_while -> for_

---
+++ fas/algorithm/for_.hpp
@@ -0,0 +1,36 @@
+//
+// Author: Vladimir Migashko <migashko@gmail.com>, (C) 2012
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+
+#ifndef FAS_ALGORITHM_FOR__HPP
+#define FAS_ALGORITHM_FOR__HPP
+
+#include <fas/algorithm/detail/for_.hpp>
+#include <fas/algorithm/algomacro.hpp>
+#include <fas/mp/lambda.hpp>
+
+namespace fas{
+
+template<typename I, typename C, typename F >
+struct for_
+{
+  typedef typename detail::for_helper_t<
+    FAS_T_SIMPLIFY(I),
+    lambda< FAS_T_SIMPLIFY(C) >::template apply,
+    lambda< FAS_T_SIMPLIFY(F) >::template apply
+  >::type type;
+};
+
+template<typename I, template<typename> class  C, template<typename> class F >
+struct for_t
+{
+  typedef typename detail::for_helper_t<
+    I, C, F
+  >::type type;
+};
+
+}
+
+#endif
