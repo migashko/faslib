38df03565b4d4c65c5ee94f59618dbf1b65fd685
migashko <migashko@gmail.com>
2012-07-10 02:54:58 +0400

    tut algo2 upd

--- tutorial/algorithm/algorithm2.cpp
+++ tutorial/algorithm/algorithm2.cpp
@@ -5,14 +5,34 @@
 
 #include <iostream>
 
+template<int i>
+struct foo;
+
+template<int i>
+struct bar;
+
+
 namespace std
 {
   template<typename L, typename R>
-  ostream& operator << ( ostream& os, fas::type_list<L,R> )  { os << L::value << ", " << R(); return os;}
+  ostream& operator << ( ostream& os, fas::type_list<L,R> )  { os << L::value  << ", " << R(); return os;}
+
+  template<int i, typename R>
+  ostream& operator << ( ostream& os, fas::type_list< foo<i>, R> )  { os << "foo-" << i << ", " << R(); return os;}
+
+  template<int i, typename R>
+  ostream& operator << ( ostream& os, fas::type_list< bar<i>, R> )  { os << "bar-" << i << ", " << R(); return os;}
+
   
   template<typename L>
-  ostream& operator << ( ostream& os, fas::type_list<L,fas::empty_list> )  { os << L::value; return os; }
-  
+  ostream& operator << ( ostream& os, fas::type_list<L, fas::empty_list> )  { os << L::value; return os; }
+
+  template<int i>
+  ostream& operator << ( ostream& os, fas::type_list<foo<i>, fas::empty_list> )  { os << "foo-" << i; return os; }
+
+  template<int i>
+  ostream& operator << ( ostream& os, fas::type_list<bar<i>, fas::empty_list> )  { os << "bar-" << i; return os; }
+
   ostream& operator << ( ostream& os, fas::empty_list )  { return os; }
 }
 
@@ -28,10 +48,16 @@ struct bar
   enum { bar_value = i};
 };
 
-template<typename T, template <int> class Fi >
-struct make
+template<typename T>
+struct make_foo
 {
-  typedef Fi<T::value> type;
+  typedef foo<T::value> type;
+};
+
+template<typename T>
+struct make_bar
+{
+  typedef bar<T::value> type;
 };
 
 template<typename T>
@@ -51,30 +77,42 @@ int main()
 {
   using fas::_;
   using fas::_1;
+  using fas::a;
+  using fas::p;
   using fas::int_;
   
-  typedef fas::tail<
-    fas::generate<
-      fas::int_<11>, 
-      fas::generator< 
-        int_<42>, 
-        fas::modulus< fas::rand<_>, int_<3> >
-      > 
-    >::type 
+  typedef fas::generate<
+    fas::int_<10>, 
+    fas::generator< 
+      fas::rand<int_<111> >, 
+      fas::make_int< fas::rand<_> >
+    > 
   >::type randlist;
   
   std::cout << "randlist: " << randlist() << std::endl;
   
+  typedef fas::transform<randlist, fas::make_int< fas::modulus<_, int_<3> > > >::type randlist_012;
+  
+  std::cout << "randlist_012: " << randlist_012() << std::endl;
+  
   typedef fas::transform_if< 
-    randlist, 
-    make<_, foo>
+    randlist_012, 
+    make_foo<_1>,
     fas::or_< 
       fas::equal_to< int_<0>, _1 >, 
       fas::equal_to< int_<2>, _1 > 
-    >  
-  >::type list1;
+    >
+  >::type foo_1_list;
   
+  std::cout << "foo_1_list: " << foo_1_list() << std::endl;
   
+  typedef fas::transform_if< 
+    foo_1_list, 
+    make_bar<_1>,
+    is_int<_1>
+  >::type foo_bar_list;
+  
+  std::cout << "foo_bar_list: " << foo_bar_list() << std::endl;
   /*
   typedef fas::generate< int_<10>, fas::generator< int_<1>, fas::inc<_> > >::type genlist;
   std::cout << "genlist: " << genlist() << std::endl;
