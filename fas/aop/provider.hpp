482faa728cf6463b94c32b3fa61f4f7a41261728
migashko <migashko@gmail.com>
2012-12-19 02:28:05 +0400

    provider upd

--- fas/aop/provider.hpp
+++ fas/aop/provider.hpp
@@ -7,11 +7,35 @@
 #ifndef FAS_AOP_PROVIDER_HPP
 #define FAS_AOP_PROVIDER_HPP
 
+#include <fas/mp/lambda.hpp>
+#include <fas/mp/w.hpp>
+
 namespace fas{
 
-template<typename M >
-struct provider
+template<template<typename> class P >
+struct provider_t
 {
+  template<typename T>
+  struct build
+  {
+    typedef P<T> type;
+  };
+
+  template<typename T>
+  typename build<T>::type operator()(T& t) const
+  {
+    return typename build<T>::type(&t);
+  }
+};
+
+template<typename M >
+struct provider;
+
+template<typename M >
+struct provider< w<M> >
+  : provider_t< lambda< M >::template apply >
+{
+  /*
   template<typename T>
   struct build
   {
@@ -23,6 +47,7 @@ struct provider
   {
     return typename build<T>::type(&t);
   }
+  */
 };
 
 }
