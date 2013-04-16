e163a6e0d4a78b879a227ddfd643c436957285d8
Vladimir Migashko <migashko@gmail.com>
2013-04-16 15:56:08 +0400

    upd

--- fas/testing/statements.hpp
+++ fas/testing/statements.hpp
@@ -52,18 +52,18 @@ struct equal<M, empty_type>: statement<M>
 template<typename M = assert, typename T = empty_type>
 struct not_equal: statement<M>
 {
-  equal(T left, T right): statement<M>( left != right, "Not equal statement. " ) {}
-  equal(T left, T right, const std::string& text): statement<M>( left != right, text ) {}
+  not_equal(T left, T right): statement<M>( left != right, "Not equal statement. " ) {}
+  not_equal(T left, T right, const std::string& text): statement<M>( left != right, text ) {}
 };
 
 template<typename M>
 struct not_equal<M, empty_type>: statement<M>
 {
   template<typename L, typename R>
-  equal(L left, R right): statement<M>( left != right, "Not equal statement. " ) {}
+  not_equal(L left, R right): statement<M>( left != right, "Not equal statement. " ) {}
 
   template<typename L, typename R>
-  equal(L left, R right, const std::string& text): statement<M>( left != right, text ) {}
+  not_equal(L left, R right, const std::string& text): statement<M>( left != right, text ) {}
 };
 
 
