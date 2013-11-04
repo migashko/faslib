d89e6124b31a0652b6eb3bdc1d6190fbba7a8241
Vladimir Migashko <migashko@gmail.com>
2013-11-04 06:51:24 +0400

    range helper upd

--- fas/range/detail/range_helper_range.hpp
+++ fas/range/detail/range_helper_range.hpp
@@ -25,16 +25,15 @@ struct range_helper<R, typerange_flag::range >
   typedef typename range::difference_type   difference_type;
   typedef typename range::iterator   iterator;
 
-  static inline R make_range(R r)
+  static inline R& make_range(R& r)
   {
     return r;
   }
 
-  static inline R make_orange(R r, bool)
+  static inline R& make_orange(R& r, bool)
   {
     return r;
   }
-
   
   template<typename RR /*==R */>
   static inline
