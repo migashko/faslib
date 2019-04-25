6a750862b1332e12a1fefe5e528c2a2807ff3817
Vladimir Migashko <migashko@gmail.com>
2019-04-26 01:31:52 +0300

    nullptr for c++98 wrn fix

--- fas/system/nullptr.hpp
+++ fas/system/nullptr.hpp
@@ -17,11 +17,11 @@ class fas_nullptr_t
 public:
    template<class T>          /* convertible to any type       */
    operator T*() const        /* of null non-member            */
-      { return 0; }           /* pointer...                    */
+      { return NULL; }           /* pointer...                    */
 
    template<class C, class T> /* or any type of null           */
       operator T C::*() const /* member pointer...             */
-      { return 0; }   
+      { return NULL; }   
 
 private:
    void operator&() const;    /* Can't take address of nullptr */
