be25e6da2db0110e15f81beaf244bbd711692b53
Vladimir Migashko <migashko@gmail.com>
2019-04-26 18:51:47 +0300

    Mambaru (#40)
    * 0.8.1

--- fas/system/nullptr.hpp
+++ fas/system/nullptr.hpp
@@ -8,25 +8,32 @@
 #define FAS_SYSTEM_NULLPTR_HPP
 
 #if __cplusplus > 199711L
-#define fas_nullptr nullptr  
-#define fas_nullptr_t nullptr_t
+  #if defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9))
+    #define fas_nullptr nullptr
+    #define fas_null_param nullptr
+    #define fas_nullptr_t nullptr_t
+  #else
+    #define fas_nullptr    nullptr
+    #define fas_null_param static_cast<void*>(nullptr)
+  #endif
 #else
-const                         /* this is a const object...     */
-class fas_nullptr_t
-{
-public:
-   template<class T>          /* convertible to any type       */
-   operator T*() const        /* of null non-member            */
-      { return NULL; }           /* pointer...                    */
+  #define fas_null_param static_cast<void*>(0)
+  const                         /* this is a const object...     */
+  class fas_nullptr_t
+  {
+  public:
+    template<class T>          /* convertible to any type       */
+    operator T*() const        /* of null non-member            */
+        { return NULL; }           /* pointer...                    */
 
-   template<class C, class T> /* or any type of null           */
-      operator T C::*() const /* member pointer...             */
-      { return NULL; }   
+    template<class C, class T> /* or any type of null           */
+        operator T C::*() const /* member pointer...             */
+        { return NULL; }   
 
-private:
-   void operator&() const;    /* Can't take address of nullptr */
+  private:
+    void operator&() const;    /* Can't take address of nullptr */
 
-} fas_nullptr = {};     
+  } fas_nullptr = {};     
 #endif
 
 #endif
