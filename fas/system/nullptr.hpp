73d83d4d0902ac5e7ab6ce76d5f19c5960a286f7
Vladimir Migashko <migashko@gmail.com>
2019-04-26 13:44:06 +0300

    *fas/system/nullptr.hpp

--- fas/system/nullptr.hpp
+++ fas/system/nullptr.hpp
@@ -8,25 +8,25 @@
 #define FAS_SYSTEM_NULLPTR_HPP
 
 #if __cplusplus > 199711L
-#define fas_nullptr nullptr  
-#define fas_nullptr_t nullptr_t
+  #define fas_nullptr nullptr  
+  #define fas_nullptr_t nullptr_t
 #else
-const                         /* this is a const object...     */
-class fas_nullptr_t
-{
-public:
-   template<class T>          /* convertible to any type       */
-   operator T*() const        /* of null non-member            */
-      { return NULL; }           /* pointer...                    */
+  const                         /* this is a const object...     */
+  class fas_nullptr_t
+  {
+  public:
+    template<class T>          /* convertible to any type       */
+    operator T*() const        /* of null non-member            */
+        { return NULL; }           /* pointer...                    */
 
-   template<class C, class T> /* or any type of null           */
-      operator T C::*() const /* member pointer...             */
-      { return NULL; }   
+    template<class C, class T> /* or any type of null           */
+        operator T C::*() const /* member pointer...             */
+        { return NULL; }   
 
-private:
-   void operator&() const;    /* Can't take address of nullptr */
+  private:
+    void operator&() const;    /* Can't take address of nullptr */
 
-} fas_nullptr = {};     
+  } fas_nullptr = {};     
 #endif
 
 #endif
