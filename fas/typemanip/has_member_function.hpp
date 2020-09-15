b0ad6a09528b337e303fc484182699211d7fdd1c
Vladimir Migashko <migashko@gmail.com>
2020-09-15 23:55:58 +0300

    Generated export autocommit from mamba.ru

--- fas/typemanip/has_member_function.hpp
+++ fas/typemanip/has_member_function.hpp
@@ -1,5 +1,5 @@
 //
-// Author: Vladimir Migashko <migashko@gmail.com>, (C) 2011
+// Author: Vladimir Migashko <migashko@gmail.com>, (C) 2011, 2020
 //
 // Copyright: See COPYING file that comes with this distribution
 //
@@ -119,6 +119,68 @@ struct N\
 {\
   typedef N##_detail::N##_helper helper; \
   enum { value = sizeof( helper::test<T>(0) ) == sizeof(helper::small_type) };\
+};
+
+//
+// Function parameter types are specified in the structure template
+//
+
+
+#define FAS_HAS_STATIC_FUNCTION0T(N, D)\
+namespace N##_detail{\
+template < typename T, typename R, R(*)() > struct op_mf {}; \
+struct N##_helper {\
+  typedef char small_type;\
+  typedef struct big_tag { char dummy[2];} big_type;\
+  template<typename P, typename R>\
+  static small_type test(op_mf<P, R, &P::D>*);\
+  template<typename P, typename R>\
+  static big_type test(...);\
+}; \
+} \
+template<typename T, typename R>\
+struct N\
+{\
+  typedef N##_detail::N##_helper helper; \
+  enum { value = sizeof( helper::test<T, R>(0) ) == sizeof(helper::small_type) };\
+};
+
+#define FAS_HAS_STATIC_FUNCTION1T(N, D)\
+namespace N##_detail{\
+template < typename T, typename R, typename P1, R (*)(P1) > struct op_mf {}; \
+struct N##_helper {\
+  typedef char small_type;\
+  typedef struct big_tag { char dummy[2];} big_type;\
+  template<typename P, typename R, typename P1>\
+  static small_type test(op_mf<P, R, P1, &P::D>*);\
+  template<typename P, typename R, typename P1>\
+  static big_type test(...);\
+}; \
+} \
+template<typename T, typename R, typename P1>\
+struct N\
+{\
+  typedef N##_detail::N##_helper helper; \
+  enum { value = sizeof( helper::test<T, R, P1>(0) ) == sizeof(helper::small_type) };\
+};
+
+#define FAS_HAS_STATIC_FUNCTION2T(N, D)\
+namespace N##_detail{\
+template < typename T, typename R, typename P1, typename P2, R (*)(P1, P2) > struct op_mf {}; \
+struct N##_helper {\
+  typedef char small_type;\
+  typedef struct big_tag { char dummy[2];} big_type;\
+  template<typename P, typename R, typename P1, typename P2>\
+  static small_type test(op_mf<P, R, P1, P2, &P::D>*);\
+  template<typename P, typename R, typename P1, typename P2>\
+  static big_type test(...);\
+}; \
+} \
+template<typename T, typename R, typename P1, typename P2>\
+struct N\
+{\
+  typedef N##_detail::N##_helper helper; \
+  enum { value = sizeof( helper::test<T, R, P1, P2>(0) ) == sizeof(helper::small_type) };\
 };
 
 #endif
