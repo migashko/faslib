1af11c733763633c96d93af25050532808f78cbe
GitLab Runner <gitlab-runner@repobuild4.lan>
2024-11-29 14:37:00 +0300

    Generated export autocommit from mamba.ru

--- tutorial/out_type_list.hpp
+++ tutorial/out_type_list.hpp
@@ -8,52 +8,52 @@
 
 namespace std
 {
-  ostream& operator << ( ostream& os, fas::empty_list );
+  inline ostream& operator << ( ostream& os, fas::empty_list );
 
   template<int N>
-  ostream& operator << ( ostream& os, fas::type_list< fas::int_<N>, fas::empty_list> );
+  inline ostream& operator << ( ostream& os, fas::type_list< fas::int_<N>, fas::empty_list> );
 
   template<int N, typename R>
-  ostream& operator << ( ostream& os, fas::type_list< fas::int_<N>, R > );
+  inline ostream& operator << ( ostream& os, fas::type_list< fas::int_<N>, R > );
   
   template<typename T, T N>
-  ostream& operator << ( ostream& os, fas::type_list< fas::integral_c<T, N>, fas::empty_list> );  
+  inline ostream& operator << ( ostream& os, fas::type_list< fas::integral_c<T, N>, fas::empty_list> );
 
   template<typename T, T N, typename R>
-  ostream& operator << ( ostream& os, fas::type_list< fas::integral_c<T, N>, R > );
+  inline ostream& operator << ( ostream& os, fas::type_list< fas::integral_c<T, N>, R > );
 }
 
 namespace std
 {
 
-  ostream& operator << ( ostream& os, fas::empty_list )  
+  inline ostream& operator << ( ostream& os, fas::empty_list )
   { 
     return os; 
   }
 
   template<int N>
-  ostream& operator << ( ostream& os, fas::type_list< fas::int_<N>, fas::empty_list> )  
+  inline ostream& operator << ( ostream& os, fas::type_list< fas::int_<N>, fas::empty_list> )
   { 
     os << N; 
     return os;
   }
 
   template<int N, typename R>
-  ostream& operator << ( ostream& os, fas::type_list< fas::int_<N>, R > )  
+  inline ostream& operator << ( ostream& os, fas::type_list< fas::int_<N>, R > )
   {
     os << N << ", " << R(); 
     return os;
   }
 
   template<typename T, T N>
-  ostream& operator << ( ostream& os, fas::type_list< fas::integral_c<T, N>, fas::empty_list> )  
+  inline ostream& operator << ( ostream& os, fas::type_list< fas::integral_c<T, N>, fas::empty_list> )
   { 
     os << N; 
     return os;
   }
 
   template<typename T, T N, typename R>
-  ostream& operator << ( ostream& os, fas::type_list< fas::integral_c<T, N>, R > )  
+  inline ostream& operator << ( ostream& os, fas::type_list< fas::integral_c<T, N>, R > )
   {
     os << N << ", " << R(); 
     return os;
